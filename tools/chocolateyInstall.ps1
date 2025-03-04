effb94bbf18f2c0e60aa33689305c55b84a3249c
GitHub Action <action@github.com>
2025-03-04 01:18:52 +0000

    Update package to version 0.6.4

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'uv'
-$url64 = 'https://github.com/astral-sh/uv/releases/download/0.6.3/uv-x86_64-pc-windows-msvc.zip'
+$url64 = 'https://github.com/astral-sh/uv/releases/download/0.6.4/uv-x86_64-pc-windows-msvc.zip'
 
-$checksum64 = '40b50b3da3cf74dc5717802acd076b4669b6d7d2c91c4482875b4e5e46c62ba3'
+$checksum64 = '4526b772f5d9b0afdd61a780ab649730d125e0a56348daa7463ee44c0c5e5762'
 
 $packageArgs = @{
   packageName   = $packageName
